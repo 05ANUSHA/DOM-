04569edbe4d40c2edaf1b84eb1cb07e87c813289
05ANUSHA <05anusha.a@gmail.com>
2024-03-21 14:15:46 +0530

    querySelector and querySelectorAll

---
+++ querySelector and querySelectorAll/index.js
@@ -0,0 +1,13 @@
+// Write the code as shown in the video below:
+// Change the color of the basket heading to brown using querySelector
+const basketHeading = document.querySelector('#basket-heading');
+basketHeading.style.color = 'brown';
+
+// Change the background color of the second list item to brown and text color to white using querySelectorAll
+const fruitItems = document.querySelectorAll('.fruit');
+fruitItems[1].style.backgroundColor = 'brown';
+fruitItems[1].style.color = 'white';
+
+// Change the background color of the fourth list item to brown and text color to white using querySelectorAll
+fruitItems[3].style.backgroundColor = 'brown';
+fruitItems[3].style.color = 'white';
