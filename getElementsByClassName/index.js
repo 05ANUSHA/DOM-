6b2cb69bed563a56923fa0445cfb014851f243a3
05ANUSHA <05anusha.a@gmail.com>
2024-03-21 13:25:06 +0530

    getElementsByClassName

---
+++ getElementsByClassName/index.js
@@ -0,0 +1,9 @@
+const fruit=document.getElementsByClassName('fruit');
+console.log(fruit[1]);
+fruit[1].style.fontWeight='bold';
+fruit[2].style.fontWeight='bold';
+fruit[3].style.fontWeight='bold';
+fruit[0].style.fontWeight='bold';
+
+const fruits = document.getElementsByClassName('fruit');
+fruits[2].style.backgroundColor = 'yellow';
